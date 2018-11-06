37e7d01aa536097fc5ba3e04f8eea140b29dae4e
yangxiaoming <2218139870@qq.com>
2018-11-06 11:42:59 +0800

    多页应用项目上传

---
+++ build/webpack.dev.conf.js
@@ -0,0 +1,63 @@
+const path = require('path')
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const portfinder = require('portfinder');
+const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin');
+const baseWebpackConfig = require('./webpack.base.conf')
+const utils = require('./utils')
+const config = require('../config/index')
+
+const devWebpackConfig = merge(baseWebpackConfig, {
+  mode: 'development',
+  module: {
+    rules: utils.styleLoaders({
+      sourceMap: config.dev.cssSourceMap,
+      usePostCSS: true
+    })
+  },
+  devServer: {
+    compress: true,
+    contentBase: false, // since we use CopyWebpackPlugin.
+    hot: true,
+    host: config.dev.host,
+    port: config.dev.port,
+    open: false,
+    overlay: true,
+    quiet: true, // necessary for FriendlyErrorsPlugin
+    historyApiFallback: {
+      rewrites: [
+        { from: /.*/, to: path.posix.join('/', 'index.html') }
+      ]
+    }
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new CopyWebpackPlugin([
+      {
+        from: 'static',
+        to: 'static',
+        ignore: ['.*', 'readme.txt']
+      }
+    ])
+  ].concat(utils.htmlPlugin())
+})
+
+module.exports = new Promise((resolve, reject) => {
+  portfinder.basePort = config.dev.port
+  portfinder.getPort((error, port) => {
+    if (error) {
+      reject(error)
+    } else {
+      devWebpackConfig.devServer.port = port
+      devWebpackConfig.plugins.push(new FriendlyErrorsPlugin({
+        compilationSuccessInfo: {
+          messages: [`Your application is running here: http://${devWebpackConfig.devServer.host}:${port}`]
+        },
+        onErrors: config.dev.notifyOnErrors ? utils.createNotifierCallback() : undefined
+      }))
+      resolve(devWebpackConfig)
+    }
+  })
+})
---
+++ src/pages/detail/detail.js
@@ -0,0 +1,3 @@
+import 'lib-flexible'
+import 'assets/less/common.less'
+import 'assets/less/detail.less'
